46d994160024c9880c2cf8b628ce468a73ed1b1e
Bar Cohen <sscohen99@gmail.com>
2023-09-06 00:18:06 +0300

    create: create prompt client side created

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -3,23 +3,51 @@
 import {Form} from '@components/Form'
 import {useState} from 'react'
 import {Post} from '@/types/formTypes'
+import {useSession} from 'next-auth/react'
+import {router} from 'next/client'
 
 const CreatePrompt = ({}) => {
+    const {data: session} = useSession()
     const [submitting, setSubmitting] = useState(false)
-    const [prompt, setPrompt] = useState<Post>({
+    const [post, setPost] = useState<Post>({
         prompt: '',
         tag: ''
     })
 
-    const createPrompt = async (event: React.FormEvent<HTMLFormElement>) => {
 
+    const createPrompt = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        setSubmitting(true)
+
+        try {
+            const res = await fetch('/api/prompt/new', {
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    tag: post.tag,
+                    user: session?.user?.email
+                })
+            })
+
+            if (res.ok) {
+                setPost({
+                    prompt: '',
+                    tag: ''
+                })
+                router.push('/')
+            }
+        } catch (e) {
+            console.error(e)
+        } finally {
+                setSubmitting(false)
+        }
     }
 
     return (
         <Form
             type={'Create'}
-            post={prompt}
-            setPrompt={setPrompt}
+            post={post}
+            setPrompt={setPost}
             submitting={submitting}
             handleSubmit={createPrompt}
         />
