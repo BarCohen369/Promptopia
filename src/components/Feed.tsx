efd9b2dcc45c2f40e9cd897507bf05cc429f8adf
Bar Cohen <sscohen99@gmail.com>
2023-09-06 18:49:26 +0300

    feed: added search field

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,5 +1,26 @@
+"use client"
+
+import {useState} from 'react'
+
 export const Feed = () => {
+    const [searchText, setSearchText] = useState('')
+    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+    }
+
     return (
-        <></>
+        <section className={'feed'}>
+            <form className={'search_form'}>
+                <input
+                    type={'text'}
+                    className={'search_input peer'}
+                    placeholder={'Search for a tag ot a user...'}
+                    required
+                    value={searchText}
+                    onChange={handleSearchChange}
+                />
+            </form>
+
+        </section>
     )
 }
