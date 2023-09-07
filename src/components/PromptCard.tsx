33dda0ffbe1462c5ed7e556d6f44980cd5ca2820
Bar Cohen <sscohen99@gmail.com>
2023-09-07 23:50:28 +0300

    profile: added edit and delete buttons, fixed buttons location

--- src/components/PromptCard.tsx
+++ src/components/PromptCard.tsx
@@ -24,43 +24,75 @@ export const PromptCard = ({post, callbacks}: CardParams) => {
 
     return (
         <li className={'prompt_card'}>
-            <section className="creator_data">
-                <div className="creator_image_wrapper">
-                    <Image
-                        src={post.creator.image || ''}
-                        alt={'user profile'}
-                        width={40}
-                        height={40}
-                        className={'rounded-full object-contain'}
-                    />
-                </div>
+            <div className="flex-between">
+                <section className="creator_data">
+                    <div className="creator_image_wrapper">
+                        <Image
+                            src={post.creator.image || ''}
+                            alt={'user profile'}
+                            width={40}
+                            height={40}
+                            className={'rounded-full object-contain'}
+                        />
+                    </div>
 
-                <div className="flex_col">
-                    <h3 className={'creator_username'}>
-                        {post.creator.username}
-                    </h3>
-                    <p className={'creator_email'}>
-                        {post.creator.email}
-                    </p>
-                </div>
-            </section>
+                    <div className="flex_col">
+                        <h3 className={'creator_username'}>
+                            {post.creator.username}
+                        </h3>
+                        <p className={'creator_email'}>
+                            {post.creator.email}
+                        </p>
+                    </div>
+                </section>
 
-            <button
-                className="copy_btn"
-                onClick={handleCopy}
-            >
-                <Image
-                    src={copied === post.prompt ?
-                        '/assets/icons/tick.svg' :
-                        '/assets/icons/copy.svg'}
-                    alt={'copy'}
-                    height={12}
-                    width={12}
-                />
-            </button>
+                <section className={'flex-end'}>
+                    <button
+                        className="copy_btn"
+                        onClick={handleCopy}
+                    >
+                        <Image
+                            src={copied === post.prompt ?
+                                '/assets/icons/tick.svg' :
+                                '/assets/icons/copy.svg'}
+                            alt={'copy'}
+                            height={12}
+                            width={12}
+                        />
+                    </button>
+
+                    {handleEdit && (
+                        <button
+                            className={'edit_btn'}
+                            onClick={handleEdit}
+                        >
+                            <Image
+                                src={'/assets/icons/edit.svg'}
+                                alt={'edit'}
+                                height={12}
+                                width={12}
+                            />
+                        </button>
+                    )}
+
+                    {handleDelete && (
+                        <button
+                            className={'delete_btn'}
+                            onClick={handleDelete}
+                        >
+                            <Image
+                                src={'/assets/icons/delete.svg'}
+                                alt={'delete'}
+                                height={12}
+                                width={12}
+                            />
+                        </button>
+                    )}
+                </section>
+            </div>
 
             <p className={'prompt'}>{post.prompt}</p>
-            <div className="tag_section">
+            <section className="tag_section">
                 {post.tags.map((tag) => (
                     <button
                         key={tag}
@@ -71,7 +103,7 @@ export const PromptCard = ({post, callbacks}: CardParams) => {
                         #{tag}
                     </button>
                 ))}
-            </div>
+            </section>
         </li>
     )
 }
