2234570a4b3e16d0b2b800357a2bc90e47a1f27a
Bar Cohen <sscohen99@gmail.com>
2023-09-07 18:17:04 +0300

    profile: added header and description to profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -11,9 +11,16 @@ type ProfileProps = {
 
 const Profile = ({type, description, data, handleEdit, handleDelete}: ProfileProps) => {
     return (
-        <div>
+        <section className={'w-full'}>
+            <h1 className={'head_text_left'}>
+                <span className={'blue_gradient'}>{type} Profile</span>
+            </h1>
+            <p className={'desc text-left'}>
+                {description}
+            </p>
 
-        </div>
+
+        </section>
     )
 }
 
